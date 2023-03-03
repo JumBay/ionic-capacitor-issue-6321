faa770fbcdb5a939d4cf2994113b1bf94df76ae0
Jean-Baptiste Malatrasi <jb.malatrasi@gmail.com>
2023-03-03 09:31:49 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-capacitor-issue-6321',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
